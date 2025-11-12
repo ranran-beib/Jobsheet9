fa13da1d00167c173e7ec84b6283d1d8eb371701
Fxzeee <firmantriwijaya3@gmail.com>
2025-11-12 08:45:19 +0700

    Modifikasi

--- ArrayBilangan05.java
+++ ArrayBilangan05.java
@@ -1,18 +1,9 @@
 public class ArrayBilangan05 {
     public static void main(String[] args) {
-        int[] bil = new int [4];
 
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        int[] bil = {5, 13, -7, 17};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-
-        System.out.println("Menampilkan dengan for-loop:");
+        System.out.println("For-loop:");
         for (int i = 0 ; i < 4 ; i++) {
             System.out.println(bil[i]);
         }
