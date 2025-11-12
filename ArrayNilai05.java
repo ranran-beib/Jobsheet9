3bcd3fdb9e82118b6fde639bb785466fdb6b1f14
Fxzeee <firmantriwijaya3@gmail.com>
2025-11-12 09:08:13 +0700

    Studi Kasus Nilai Mahasiswa di SIAKAD

---
+++ ArrayNilai05.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ArrayNilai05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int[] nilai = new int[10];
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai ke-" + i + ": ");
+            nilai[i] = input.nextInt();
+        }
+        
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai ke-" + i + ": " + nilai[i]);
+        }
+        
+        input.close();
+    }
+}
