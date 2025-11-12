9c8e658886723f671f4fc64ed535d075d57a6f32
Fxzeee <firmantriwijaya3@gmail.com>
2025-11-12 09:11:21 +0700

    Modifikasi

--- ArrayNilai05.java
+++ ArrayNilai05.java
@@ -5,14 +5,18 @@ public class ArrayNilai05 {
         
         int[] nilai = new int[10];
         
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilai.length; i++) {
             System.out.print("Masukkan nilai ke-" + i + ": ");
             nilai[i] = input.nextInt();
         }
         
         System.out.println();
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai ke-" + i + ": " + nilai[i]);
+        for (int i = 0; i < nilai.length; i++) {
+            if (nilai[i] >= 70) {
+                System.out.println("Nilai ke-" + i + " (" + nilai[i] + ") : Lulus");
+            } else {
+                System.out.println("Nilai ke-" + i + " (" + nilai[i] + ") : Tidak Lulus");
+            }
         }
         
         input.close();
